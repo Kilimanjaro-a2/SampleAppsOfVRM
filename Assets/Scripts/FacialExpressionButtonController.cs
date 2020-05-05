71a6b053f2b8e2ac6ff3b59925f549a2be07f01f
Kili <kilimanjaro.a2@gmail.com>
2020-05-05 22:49:22 +0900

    Implemented a facial expression scene

---
+++ Assets/Scripts/FacialExpressionButtonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using VRM;
+using UnityEngine.UI;
+
+public class FacialExpressionButtonController : MonoBehaviour
+{
+    public BlendShapePreset Preset;
+    public event Action<BlendShapePreset> OnFacialExpressionButtonPressed;
+
+    public void SetText(string text)
+    {
+        GetComponentInChildren<Text>().text = text;
+    }
+
+    public void OnButtonPressed()
+    {
+        OnFacialExpressionButtonPressed?.Invoke(Preset);
+    }
+}
---
+++ Assets/Scripts/FacialExpressionController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRM;
+
+public class FacialExpressionController : MonoBehaviour
+{
+    protected VRMBlendShapeProxy _blendShapeProxy;
+    public List<BlendShapeClip> GetClips()
+    {
+        if (_blendShapeProxy == null)
+        {
+            _blendShapeProxy = GetComponent<VRMBlendShapeProxy>();
+        } 
+        if (_blendShapeProxy.BlendShapeAvatar.Clips.Count == 0)
+        {
+            _blendShapeProxy.BlendShapeAvatar.CreateDefaultPreset();
+        }
+        return _blendShapeProxy.BlendShapeAvatar.Clips;
+    }
+    public void SetPreset(BlendShapePreset preset)
+    {
+        StartCoroutine(SetBlendShape(preset));
+    }
+    protected IEnumerator SetBlendShape(BlendShapePreset preset)
+    {
+        yield return new WaitForEndOfFrame();
+        ResetBlendShape();
+        yield return new WaitForEndOfFrame();
+        _blendShapeProxy.ImmediatelySetValue(preset, 1.0f);
+    }
+    protected void ResetBlendShape()
+    {
+        foreach(var clip in _blendShapeProxy.BlendShapeAvatar.Clips)
+        {
+            _blendShapeProxy.ImmediatelySetValue(clip.Preset, 0f);
+        }
+    }
+}
