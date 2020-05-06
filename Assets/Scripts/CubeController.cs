2df6133347593c4e7d1adcbe5f9ad585dbdeaa62
Kili <kilimanjaro.a2@gmail.com>
2020-05-06 13:31:02 +0900

    Enabled to destroy enemies

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    protected bool _isBroken = false;
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Attack" && !_isBroken)
+        {
+            StartCoroutine(BrokenEffect());
+        }
+    }
+    protected IEnumerator BrokenEffect()
+    {
+        _isBroken = true;
+        var go = Instantiate(Resources.Load("DestroyParticle")) as GameObject;
+        go.transform.position = transform.position;
+        Destroy(go, 2f);
+        yield return new WaitForEndOfFrame();
+        Destroy(gameObject);
+    }
+}
