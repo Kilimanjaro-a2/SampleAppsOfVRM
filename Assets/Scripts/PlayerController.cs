2dac3561c5e4df0e7fff24fc314d6f070f03d33e
Kili <kilimanjaro.a2@gmail.com>
2020-05-06 13:22:26 +0900

    Enabled to run around the field

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    protected Animator _animator;
+    protected readonly float _moveSpeed = 10f;
+    protected bool _lookLeft = false;
+    void Update()
+    {
+        var moveFlag = false;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0, -90, 0);
+            moveFlag = true;
+            _lookLeft = true;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 90, 0);
+            moveFlag = true;
+            _lookLeft = false;
+        }
+
+        if (moveFlag)
+        {
+            var moveValue = Vector3.forward * _moveSpeed;
+            transform.Translate(moveValue * Time.deltaTime);
+            _animator.SetFloat("Move", Mathf.Abs(moveValue.magnitude));
+        }
+        else
+        {
+            _animator.SetFloat("Move", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("Punch");
+            var particle = Instantiate(Resources.Load("AttackParticle")) as GameObject;
+            particle.transform.position += transform.position + new Vector3(_lookLeft ? -1f : 1f, 1f, 0);
+            Destroy(particle, 2f);
+        }
+
+        var pos = transform.position;
+        if (pos.x > 5)
+        {
+            pos.x = 5;
+            transform.position = pos;
+        }
+        if (pos.x < -5)
+        {
+            pos.x = -5;
+            transform.position = pos;
+        }
+    }
+
+    public void SetAnimatorController(RuntimeAnimatorController rac)
+    {
+        _animator = GetComponent<Animator>();
+        _animator.runtimeAnimatorController = rac;
+    }
+
+    protected IEnumerator OnDamaged()
+    {
+        _animator.SetTrigger("Damaged");
+        yield return new WaitForSeconds(1f);
+    }
+}
---
+++ Assets/Scripts/SimpleGameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGameManager : MonoBehaviour
+{
+    [SerializeField] protected GameObject _loadVRMButton;
+    [SerializeField] protected GameObject _gameStartButton;
+    [SerializeField] protected RuntimeAnimatorController _animatorController;
+    protected GameObject _playerModel;
+    void Awake()
+    {
+        var loadingManager = GetComponent<VRMLoadManager>();
+        loadingManager.OnModelLoaded += Initialize;
+    }
+
+    protected void Initialize(GameObject model)
+    {
+        _playerModel = model;
+        var script = _playerModel.AddComponent<PlayerController>();
+        script.SetAnimatorController(_animatorController);
+
+        _loadVRMButton.SetActive(false);
+        _gameStartButton.SetActive(true);
+    }
+}
