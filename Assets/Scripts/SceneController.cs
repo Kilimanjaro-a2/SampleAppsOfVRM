dee103c1ac6027cafe27fd40b67a66d474f59657
Kili <kilimanjaro.a2@gmail.com>
2020-05-06 15:54:10 +0900

    Added a scene move function

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] protected List<string> _sceneNames;
+    [SerializeField] protected int _currentIndex;
+    [SerializeField] protected Text _currentSceneText;
+    protected bool _hasPressed;
+    void Awake()
+    {
+        _currentSceneText.text = _sceneNames[_currentIndex];
+    }
+
+    public void MovePrevious()
+    {
+        _currentIndex--;
+        if (_currentIndex < 0)
+        {
+            _currentIndex = _sceneNames.Count - 1;
+        }
+        MoveScene(_currentIndex);
+    }
+
+    public void MoveNext()
+    {
+        _currentIndex++;
+        if (_currentIndex >= _sceneNames.Count)
+        {
+            _currentIndex = 0;
+        }
+        MoveScene(_currentIndex);
+    }
+
+
+    protected void MoveScene(int index)
+    {
+        if (!_hasPressed)
+        {
+            _hasPressed = true;
+            SceneManager.LoadScene(_sceneNames[index]);
+        }
+    }
+}
